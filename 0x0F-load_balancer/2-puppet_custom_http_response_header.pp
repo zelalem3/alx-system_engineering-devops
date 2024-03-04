be232e0dfd21e387c5a7e07e24622db96ebb3ed0
zelalem3 <zgetnet24@gmail.com>
2024-03-04 04:30:43 -0800

    puppet custom http response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,11 +1,29 @@
-class { 'nginx':
-  service_ensure => 'running',
-  service_enable => true,
-}
+# Custom HTTP header in a nginx server
 
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure => 'file',
-  content => "add_header X-Served-By $::hostname;",
-  require => Class['nginx'],
-  notify => Service['nginx'],
+# update ubuntu server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
+}
+->
+# install nginx web server on server
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
+}
+->
+# custom Nginx response header (X-Served-By: hostname)
+file_line { 'add HTTP header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Served-By $hostname;'
+}
+->
+# start service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
 }
